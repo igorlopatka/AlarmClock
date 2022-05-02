b83ec1b6d23740c0817f1b62d498b594fabbcaca
Igor Łopatka <igor.lopatka11@gmail.com>
2022-05-02 19:42:31 +0200

    Initial Commit

---
+++ AlarmClock/AlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockApp.swift
+//  AlarmClock
+//
+//  Created by Igor Łopatka on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlarmClock
+//
+//  Created by Igor Łopatka on 02/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
